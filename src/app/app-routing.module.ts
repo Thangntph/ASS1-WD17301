715025a1468fb5016c7f14da8b0c2a28456348a9
Nguyen Tien Thang <thangntph25654@fpt.edu.vn>
2023-05-27 13:29:20 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BaseLayoutComponent } from './layouts/base-layout/base-layout/base-layout.component';
+import { HomepageComponent } from './home/homepage/homepage.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail/product-detail.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout/admin-layout.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard/dashboard.component';
+import { ProductListComponent } from './components/product-list/product-list/product-list.component';
+import { ProductAddComponent } from './pages/product-add/product-add/product-add.component';
+import { ProductEditComponent } from './pages/product-edit/product-edit/product-edit.component';
+import { PageNotFoundComponent } from './pages/page-not-foud/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: BaseLayoutComponent, children: [
+      { path: "", component: HomepageComponent },
+      { path: "product/:id", component: ProductDetailComponent }
+    ]
+  },
+  {
+    path: 'admin', component: AdminLayoutComponent, children: [
+      { path: "", redirectTo: "dashboard", pathMatch: "full" },
+      { path: "dashboard", component: DashboardComponent },
+      { path: "product", component: ProductListComponent },
+      { path: "product/add", component: ProductAddComponent },
+      { path: "product/:id/edit", component: ProductEditComponent }
+    ]
+  },
+  { path: "**", component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list/product-list.component';
+import { HomepageComponent } from './home/homepage/homepage.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout/admin-layout.component';
+import { BaseLayoutComponent } from './layouts/base-layout/base-layout/base-layout.component';
+import { ProductEditComponent } from './pages/product-edit/product-edit/product-edit.component';
+import { ProductAddComponent } from './pages/product-add/product-add/product-add.component';
+import { PageNotFoundComponent } from './pages/page-not-foud/page-not-found/page-not-found.component';
+import { HomePageComponent } from './pages/home-page/home-page/home-page.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard/dashboard.component';
+import { AdminProductComponent } from './pages/admin/admin-product/admin-product/admin-product.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail/product-detail.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    HomepageComponent,
+    AdminLayoutComponent,
+    BaseLayoutComponent,
+    ProductEditComponent,
+    ProductAddComponent,
+    PageNotFoundComponent,
+    HomePageComponent,
+    DashboardComponent,
+    AdminProductComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
